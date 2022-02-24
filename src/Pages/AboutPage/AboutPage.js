01c739007b09e76df5132db886187a2ef8f3c28c
Jennifer Adams <wordsword23@gmail.com>
2022-02-23 22:40:27 -0500

    tweaked about page styles

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -7,7 +7,8 @@ export default class AboutPage extends React.Component {
         <h2>About This App</h2>
         <p>Generate random astronomy photos from NASA! Data and images are from NASA's Astronomy Picture of the Day (APOD) website.</p>
         <h2>About APOD</h2>
-        <p>"Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer." -- <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">Astronomy Picture of the Day</a></p>
+        <p>"Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer." </p>
+        <cite>-- <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">Astronomy Picture of the Day</a></cite>
         <p>Want to use a neat API from NASA for your own project? Visit the <a href="https://api.nasa.gov/" target="_blank">NASA API portal</a>.</p>
         <h2>About the Developer</h2>
         <p>Jennifer Adams is a Pittsburgh-based web developer with a passion for astronomy.</p>
