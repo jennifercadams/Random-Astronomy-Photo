0a1e39e8f75caacdfb5c6aa340b8c87a71360718
Jennifer Adams <wordsword23@gmail.com>
2022-02-25 21:47:06 -0500

    hid My Photos link on AboutPage

--- src/Components/Header/Nav/Nav.js
+++ src/Components/Header/Nav/Nav.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
-export default class Nav extends React.Component {
-  render() {
-    return (
-      <ul>
-        <li><NavLink to="/Random-Astronomy-Photo/random">Random</NavLink></li>
-        <li><NavLink to="/Random-Astronomy-Photo/by-date">By Date</NavLink></li>
-        <li><NavLink to="/Random-Astronomy-Photo/about">About</NavLink></li>
-        <li><a href="#history">My Photos</a></li>
-      </ul>
-    )
-  }
+export default function Nav() {
+  const location = useLocation();
+  const path = location.pathname;
+  return (
+    <ul>
+      <li><NavLink to="/Random-Astronomy-Photo/random">Random</NavLink></li>
+      <li><NavLink to="/Random-Astronomy-Photo/by-date">By Date</NavLink></li>
+      <li><NavLink to="/Random-Astronomy-Photo/about">About</NavLink></li>
+      {path !== "/Random-Astronomy-Photo/about" && <li><a href="#history">My Photos</a></li>}
+    </ul>
+  )
 }
